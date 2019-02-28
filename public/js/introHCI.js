856927085fbe0015e4889579a47c61edb15d37e9
mkhawark <mkhawar@ucsd.edu>
2019-02-28 10:45:56 -0800

    lab 8 commit

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+ 	//initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
@@ -19,9 +19,29 @@ function initializePage() {
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	// $(function() {
+	// 	$(".judge-img").bind("taphold", tapholdHandler);
+	// 	function tapholdHandler(event) {
+	// 		//get id of event source 
+	// 		var targetIDPrefix = event.target.id;
+	// 		console.log("got prefix:" + targetIDPrefix);
+	// 		//show bio
+	// 		$("#" + targetIDPrefix + "-bio").show();
+	// 	}
+	// });
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+ //  $('#rsvpForm').submit(function(e) {
+	// 	e.preventDefault();
+	// 	console.log("Submitting form...");
+	// 	var rsvpEmail = $('#rsvpEmail').val();
+	// 	$.post('addRSVP', {rsvpEmail:rsvpEmail}, postCallback);
+	// });
+	// function postCallback(res) {
+	// 	alert("RSVP was successfully submitted!");
+	// 	$('#rsvpEmail').val('');
+	// }
 }
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'nrneUb5DTY6eq2ZsG6pkvrgMgPkc4xZP';
+	var ucsd_ltlng = {lat: 32.8788033, lng: -117.2359121};
+
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		zoom: 12
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
